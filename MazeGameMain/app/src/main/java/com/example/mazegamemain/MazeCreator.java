f7297f711aa2ee16d1c523f42657c9695fd0db57
yyyicheng <48524355+yyyicheng@users.noreply.github.com>
2019-04-30 14:28:50 +0200

    re-design the maze

--- MazeGameMain/app/src/main/java/com/example/mazegamemain/MazeCreator.java
+++ MazeGameMain/app/src/main/java/com/example/mazegamemain/MazeCreator.java
@@ -6,84 +6,40 @@ public class MazeCreator {
 
     public static MazeGame adaptMaze(int mazeNo) {
         MazeGame maze = null;
-        //int[][] mazeNu = new int[][];
         if (mazeNo == 1) {
             maze = new MazeGame();
             mazeNu = new int[][]{
                     {0, 0, 0, 0, 0, 0, 1, 0, 0, 2},
                     {0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
-                    {0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
-                    {0, 0, 0, 0, 1, 1, 1, 1, 1, 0},
+                    {0, 0, 0, 0, 1, 0, 1, 0, 0, 0},
+                    {0, 0, 0, 0, 1, 0, 1, 1, 1, 0},
                     {0, 0, 0, 0, 1, 0, 0, 0, 1, 0},
+                    {0, 0, 0, 0, 1, 0, 0, 0, 1, 0},
+                    {0, 0, 0, 3, 1, 0, 0, 0, 1, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
                     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                    {0, 0, 0, 3, 0, 0, 0, 0, 0, 0},
-                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 1}};
 
-//            boolean[][] vLines = new boolean[][]{
-//                    {true ,false,false,false,true ,false,false},
-//                    {true ,false,false,true ,false,true ,true },
-//                    {false,true ,false,false,true ,false,false},
-//                    {false,true ,true ,false,false,false,true },
-//                    {true ,false,false,false,true ,true ,false},
-//                    {false,true ,false,false,true ,false,false},
-//                    {false,true ,true ,true ,true ,true ,false},
-//                    {false,false,false,true ,false,false,false}
-//            };
-//            boolean[][] hLines = new boolean[][]{
-//                    {false,false,true ,true ,false,false,true ,false},
-//                    {false,false,true ,true ,false,true ,false,false},
-//                    {true ,true ,false,true ,true ,false,true ,true },
-//                    {false,false,true ,false,true ,true ,false,false},
-//                    {false,true ,true ,true ,true ,false,true ,true },
-//                    {true ,false,false,true ,false,false,true ,false},
-//                    {false,true ,false,false,false,true ,false,true }
             maze.setLines(mazeNu);
             maze.setStartPosition(6, 3);
             maze.setFinalPosition(0, 9);
 
         }
-        // maze.setVerticalLines(vLines);
-        //maze.setHorizontalLines(hLines);
-
-
+        
         if (mazeNo == 2) {
             maze = new MazeGame();
             mazeNu = new int[][]{
-                    {1, 0, 1, 0, 0, 0, 1, 0, 0, 2},
-                    {1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+                    {1, 1, 1, 0, 0, 0, 1, 0, 0, 2},
+                    {1, 1, 1, 0, 1, 0, 1, 0, 1, 0},
                     {1, 0, 0, 0, 1, 0, 1, 0, 0, 0},
                     {1, 0, 1, 1, 1, 0, 1, 1, 1, 0},
                     {1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
                     {1, 0, 1, 1, 1, 1, 1, 0, 0, 0},
-                    {1, 1, 1, 0, 1, 0, 1, 1, 0, 1},
+                    {1, 0, 1, 0, 0, 0, 1, 1, 0, 1},
                     {1, 0, 1, 0, 1, 0, 0, 0, 0, 0},
                     {1, 0, 1, 0, 1, 1, 1, 1, 1, 0},
                     {3, 0, 0, 0, 0, 0, 1, 0, 0, 0}};
 
-//            boolean[][] vLines = new boolean[][]{
-//                    {false,false,false,true ,false,false,false},
-//                    {false,false,true ,false,true ,false,false},
-//                    {false,false,true ,true ,false,false,false},
-//                    {false,false,true ,true ,true ,false,false},
-//                    {false,false,true ,false,true ,false,false},
-//                    {true ,false,false,true ,false,true ,false},
-//                    {true ,false,true ,true ,false,false,false},
-//                    {false,false,true ,false,false,false,true }
-//            };
-//            boolean[][] hLines = new boolean[][]{
-//                    {false,true ,true ,true ,false,true ,true ,true },
-//                    {true ,true ,false,false,true ,true ,true ,false},
-//                    {false,true ,true ,false,false,false,true ,true },
-//                    {true ,true ,false,false,false,true ,true ,false},
-//                    {false,true ,true ,true ,true ,false,true ,false},
-//                    {false,false,true ,false,false,true ,true ,true },
-//                    {false,true ,false,false,true ,true ,false,false}
-//            };
-
-            // maze.setVerticalLines(vLines);
-            //maze.setHorizontalLines(hLines);
             maze.setLines(mazeNu);
             maze.setStartPosition(9, 0);
             maze.setFinalPosition(0, 9);
@@ -108,37 +64,6 @@ public class MazeCreator {
                     {0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0},
                     {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0}};
 
-//           boolean[][] vLines = new boolean[][]{
-//                    {false,false,true ,false,false,false,true ,false,false,false,false,false},
-//                    {false,true ,false,false,false,true ,false,false,false,false,true ,true },
-//                    {true ,false,false,false,false,true ,false,false,false,false,true ,true },
-//                    {true ,true ,false,false,false,true ,true ,true ,false,false,true ,true },
-//                    {true ,true ,true ,false,false,true ,true ,false,true ,false,true ,true },
-//                    {false,true ,true ,true ,false,true ,false,false,false,true ,false,false},
-//                    {false,false,false,true ,false,true ,false,true ,false,false,false,false},
-//                    {false,false,true ,false,true ,false,true ,true ,false,true ,false,false},
-//                    {true ,true ,true ,true ,false,true ,true ,false,false,true ,false,false},
-//                    {false,false,false,true ,false,false,true ,true ,false,true ,true ,false},
-//                    {false,false,true ,false,true ,false,true ,false,false,false,false,false},
-//                    {true ,true ,true ,true ,true ,true ,true ,false,false,true ,false,false},
-//                    {false,false,true ,false,false,true ,false,false,false,false,true ,false}
-//            };
-//            boolean[][] hLines = new boolean[][]{
-//                    {true ,false,false,true ,true ,false,false,false,true ,true ,true ,true ,false},
-//                    {false,true ,true ,true ,true ,true ,true ,true ,true ,true ,false,false,false},
-//                    {false,false,true ,true ,true ,false,false,true ,true ,true ,true ,false,false},
-//                    {false,false,false,true ,true ,true ,false,false,false,true ,false,false,false},
-//                    {false,false,false,false,true ,false,false,true ,true ,true ,false,false,false},
-//                    {true ,true ,false,false,false,true ,true ,true ,true ,false,true ,true ,true },
-//                    {false,true ,true ,true ,true ,true ,false,false,false,true ,true ,true ,false},
-//                    {true ,false,false,false,true ,false,true ,false,true ,false,false,true ,true },
-//                    {false,true ,false,false,false,true ,false,true ,true ,true ,true ,true ,false},
-//                    {true ,true ,false,true ,false,true ,true ,false,false,true ,false,true ,false},
-//                    {false,true ,true ,false,true ,false,false,true ,true ,false,true ,true ,true },
-//                    {false,true ,false,false,true ,false,false,true ,true ,true ,false,false,true }
-//            };
-//          maze.setVerticalLines(vLines);
-            //maze.setHorizontalLines(hLines);
             maze.setLines(mazeNu);
             maze.setStartPosition(0, 0);
             maze.setFinalPosition(0, 13);
